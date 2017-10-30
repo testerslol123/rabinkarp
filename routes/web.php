06781c53daa946d588696f989e332286cd2a61ee
Danny <danny@expecto.io>
2017-10-31 02:32:55 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('login', function() {
+    return view('login');
+});
+
+Route::get('user', function() {
+    return view('user');
+});
+
+Route::get('register', function() {
+    return view('register');
 });
